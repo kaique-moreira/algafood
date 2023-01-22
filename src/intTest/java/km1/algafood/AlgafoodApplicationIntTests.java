0b082d4e781b0096314a4f54e1dee8bb025f94b7
moreira-ks <kaique.moreira.dev@gmail.com>
2023-01-22 17:18:31 -0300

    feat(intTest): setup to integration tests

--- src/intTest/java/km1/algafood/AlgafoodApplicationIntTests.java
+++ src/intTest/java/km1/algafood/AlgafoodApplicationIntTests.java
@@ -2,8 +2,10 @@ package km1.algafood;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AlgafoodApplicationIntTests {
 
 	@Test
