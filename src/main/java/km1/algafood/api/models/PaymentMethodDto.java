8868011a8805d7dd7fd5a3b65fa3476b38c5ac8b
moreira-ks <kaique.moreira.dev@gmail.com>
2023-01-31 17:21:19 -0300

    feat(payment-method-api): create dtos models

---
+++ src/main/java/km1/algafood/api/models/PaymentMethodDto.java
@@ -0,0 +1,15 @@
+package km1.algafood.api.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentMethodDto {
+  Long id;
+  String description;
+}
