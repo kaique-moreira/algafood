b63a81c552a6d91b95a90810c9b055bd0cac9722
Kaique Moreira da Silva <kaique.moreira.dev@gmail.com>
2023-11-09 14:37:08 -0300

    wip: fix tests in controller layer

--- src/main/java/km1/algafood/api/models/CityInput.java
+++ src/main/java/km1/algafood/api/models/CityInput.java
@@ -1,10 +1,12 @@
 package km1.algafood.api.models;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class CityInput {
   private String name;
   private StateIdImput state;
--- src/main/java/km1/algafood/api/models/CuisineDto.java
+++ src/main/java/km1/algafood/api/models/CuisineDto.java
@@ -2,11 +2,13 @@ package km1.algafood.api.models;
 
 import java.util.List;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class CuisineDto {
   private Long id;
   private String name;
