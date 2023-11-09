9f7fb7598cf590638eb69b0165c82f602208656a
Kaique Moreira da Silva <kaique.moreira.dev@gmail.com>
2023-11-09 14:35:30 -0300

    start spring boot application

---
+++ src/main/java/km1/algafood/AlgafoodApplication.java
@@ -0,0 +1,13 @@
+package km1.algafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafoodApplication.class, args);
+	}
+
+}
